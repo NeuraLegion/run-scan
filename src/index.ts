731af3d5adacdf2cc079f326a4b21c859cea47a4
UncleVic <vic.polyakov@gmail.com>
2021-09-21 17:57:44 +0300

    fix: run creating scan

--- src/index.ts
+++ src/index.ts
@@ -12,20 +12,20 @@ const hostsFilter = getArray("hosts_filter");
 const type = core.getInput("type");
 const hostname = core.getInput("hostname");
 
-function getArray(name: string): string[] | null {
+function getArray(name: string): string[] | undefined {
   const input = core.getInput(name);
 
   try {
     const elements = JSON.parse(input);
 
     if (elements instanceof Array) {
-      return elements.length == 0 ? null : elements;
+      return elements.length == 0 ? undefined : elements;
     } else {
-      return null;
+      return undefined;
     }
   } catch (err) {
     core.debug(name + ` failed: ${err}` + " => " + input);
-    return null;
+    return undefined;
   }
 }
 
@@ -70,55 +70,61 @@ async function retest(token: string, uuid: string, name?: string) {
       }
     }
   } catch (err) {
-    core.setFailed(`Failed: ${err}`);
+    core.setFailed(`Failed run retest: ${err}`);
   }
 }
 
 interface NewScan {
   name: string;
-  discoveryTypes: string[] | null;
-  module: string | null;
-  crawlerUrls: string[] | null;
-  fileId: string | null;
-  hostsFilter: string[] | null;
+  discoveryTypes: string[];
+  module?: string;
+  crawlerUrls?: string[];
+  fileId?: string;
+  hostsFilter?: string[];
 }
 
 async function create(token: string, scan: NewScan) {
+  let restRes: rm.IRestResponse<Scan>;
   try {
     console.debug(scan);
     let options = { additionalHeaders: { Authorization: `Api-Key ${token}` } };
-    let restRes: rm.IRestResponse<Scan> = await restc.create<Scan>(
+    restRes = await restc.create<Scan>(
       "api/v1/scans/",
       scan,
       options
     );
 
-    switch (restRes.statusCode) {
-      case 201: {
-        let id = restRes.result!.id;
-        let url = `https://nexploit.app/scans/${id}`;
-        console.log(`Success. Scan was created on ${url}`);
-        core.setOutput("url", url);
-        core.setOutput("id", id);
-        return;
-      }
-      case 400: {
-        core.setFailed("Failed to run scan");
-        return;
-      }
-      case 401: {
-        core.setFailed("Failed to log in with provided credentials");
-        return;
-      }
-      case 403: {
-        core.setFailed(
-          "The account doesn't have any permissions for a resource"
-        );
-        return;
+    if (restRes.statusCode < 300) {
+      let id = restRes.result!.id;
+      let url = `https://nexploit.app/scans/${id}`;
+      console.log(`Success. Scan was created on ${url}`);
+      core.setOutput("url", url);
+      core.setOutput("id", id);
+    } else {
+      core.setFailed(`Failed create scan. Status code: ${restRes.statusCode}`);
+    }
+  } catch (err: any) {
+    if ('statusCode' in err) {
+      switch (err.statusCode) {
+        case 400: {
+          core.setFailed(`Failed to run scan: ${err.message}`);
+          return;
+        }
+        case 401: {
+          core.setFailed(`Failed to log in with provided credentials: ${err.message}`);
+          return;
+        }
+        case 403: {
+          core.setFailed(
+            `The account doesn't have any permissions for a resource: ${err.message}`
+          );
+          return;
+        }
+        default: {
+          core.setFailed(`Failed create scan: ${err.message}`);
+        }
       }
     }
-  } catch (err) {
-    core.setFailed(`Failed: ${err}`);
   }
 }
 
@@ -140,7 +146,7 @@ if (restartScanID) {
     );
   }
 } else {
-  const module = module_in || "core";
+  const module = module_in || "dast";
   const discoveryTypes = discoveryTypes_in || ["archive"];
 
   create(apiToken, {
